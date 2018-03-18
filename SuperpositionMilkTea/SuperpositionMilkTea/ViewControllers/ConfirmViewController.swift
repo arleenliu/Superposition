503ffeba24467c79904d1a83c9279a6f1b42d0c1
Jannie Zhong <janniezhong@jannies-macbook-pro.local>
2018-03-18 08:14:12 -0700

    s

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/ConfirmViewController.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/ConfirmViewController.swift
@@ -16,8 +16,20 @@ class ConfirmViewController : UIViewController /*UITableViewDelegate, UITableVie
         self.title = "Confirmation"
         
         let centerMessage = UILabel(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        centerMessage.textAlignment = .center
+        centerMessage.text = "Your order is complete!"
+        self.view.addSubview(centerMessage)
+        
+    
+        let pay = UIButton(frame: CGRect(x: (UIScreen.main.bounds.width / 2) - 50, y: (UIScreen.main.bounds.height * 3 / 4), width: 100, height: 60))
+        pay.backgroundColor = UIColor.blue
+        pay.layer.cornerRadius = 3.0
+        pay.setTitle("Pay for your drink", for: .normal)
+        self.view.addSubview(pay)
+        }
         
         
-    }
+        
+        
     
 }
