2c2c8e812d7a2a44a04d87971a0d9bbb44b90be8
Arleen Liu <arleenliu@gmail.com>
2018-03-18 01:53:02 -0700

    yay finally something works

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/MenuViewController.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/MenuViewController.swift
@@ -9,10 +9,41 @@
 import Foundation
 import UIKit
 
-class MenuViewController : UIViewController /*UITableViewDelegate, UITableViewDataSource*/ {
+public var entries: [(String)] = ["Order", "Profile", "Browse", "Settings", "Logout"]
+
+class MenuViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Menu"
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //public var entries: [(String, UIColor)] = []
+        let entry = entries[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = entry
+        cell.backgroundColor = UIColor.green
+        return cell
+    }
+    
     
 }
