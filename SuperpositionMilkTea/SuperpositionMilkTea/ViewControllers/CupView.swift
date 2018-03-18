c9ad56e2c71d84509d3d24666b133c501ac4f25d
Jannie Zhong <janniezhong@Jannies-MacBook-Pro.local>
2018-03-18 01:00:52 -0700

    lalalalal i hate my life

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/CupView.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/CupView.swift
@@ -9,7 +9,52 @@
 import Foundation
 import UIKit
 
+//let cup = CGRect(x: UIScreen.main.bounds.width*1.0/3, y: UIScreen.main.bounds.height*1/5, width: UIScreen.main.bounds.width*1.0/3, height: UIScreen.main.bounds.height*3/5)
+//let myView = UIView(frame: cup);
+
+
 class CupView : UIView /*UITableViewDelegate, UITableViewDataSource*/ {
+    
+    let frameY = Double(UIScreen.main.bounds.height*1/5)
+    let frameX = Double(UIScreen.main.bounds.width*1.0/3)
+    let frameHeight = Double(UIScreen.main.bounds.height*3/5)
+    let frameWidth = Double(UIScreen.main.bounds.width*1.0/3)
+    lazy var currX = frameX
+    lazy var currY = frameY + frameHeight
+    lazy var currHeight = 0.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: frameX, y: frameY, width: frameWidth, height: frameHeight))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func drawRect(percent: Double, color: UIColor) {
+        
+        currHeight = (percent / 100.0)*frameHeight
+        currY = currY-currHeight
+        
+        let rect = CGRect(x: currX, y: currY, width: frameWidth, height: currHeight)
+        let myView = UIView(frame: rect)
+        
+        
+        
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        
+        let fillColor = UIColor(red: 0.00, green: 0.59, blue: 1.0, alpha: 1.0)
+        fillColor.setFill()
+    }
+    
+//    override func draw(percent: Double, whichChoice: Int){
+//        super.draw(<#T##CGRect#>);
+//    }
+    
     
     
 }
