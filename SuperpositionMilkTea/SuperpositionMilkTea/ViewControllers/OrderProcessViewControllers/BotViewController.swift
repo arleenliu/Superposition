24327b974c77b0022f70aaaecad2ba7ade6b783c
Jannie Zhong <janniezhong@Jannies-MacBook-Pro.local>
2018-03-18 07:46:13 -0700

    wheee

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/OrderProcessViewControllers/BotViewController.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/OrderProcessViewControllers/BotViewController.swift
@@ -12,13 +12,19 @@ import UIKit
 class BotViewController : UIViewController /*UITableViewDelegate, UITableViewDataSource*/ {
     
     var pearlButton = UIButton()
+    var jellyButton = UIButton()
     var cup = CupView()
+    var aiyuButton = UIButton()
+    var puddingButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Bottom Choice"
         self.view.backgroundColor = UIColor.white
         setPearlButton()
+        setJellyButton()
+        setAiyuButton()
+        setPuddingButton()
         setupSlider()
         
         let addEntryButton = UIBarButtonItem(title: "Next", style: UIBarButtonItemStyle .plain, target: self, action: #selector(addEntryAction))
@@ -37,13 +43,40 @@ class BotViewController : UIViewController /*UITableViewDelegate, UITableViewDat
     
     // put setValues in here to run
     func setPearlButton() {
-        pearlButton = UIButton(frame: CGRect(x: ChoicesViewController.width / 5.0, y: ChoicesViewController.height / 7.0, width: ChoicesViewController.width / 5.0, height: ChoicesViewController.height / 7.0))
-        pearlButton.backgroundColor = UIColor.yellow
+        pearlButton = UIButton(frame: CGRect(x: ChoicesViewController.width / 12.0, y: ChoicesViewController.height / 7.0, width: ChoicesViewController.width / 5.0, height: ChoicesViewController.height / 7.0))
+        pearlButton.backgroundColor = UIColor.blue
         pearlButton.layer.cornerRadius = 3.0
         pearlButton.setTitle("Pearls", for: .normal)
         pearlButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         self.view.addSubview(pearlButton)
     }
+    
+    func setAiyuButton() {
+        aiyuButton = UIButton(frame: CGRect(x: ChoicesViewController.width * 9 / 12, y: ChoicesViewController.height * 4 / 7.0, width: ChoicesViewController.width / 5.0, height: ChoicesViewController.height / 7.0))
+        aiyuButton.backgroundColor = UIColor.blue
+        aiyuButton.layer.cornerRadius = 3.0
+        aiyuButton.setTitle("Ai-yu", for: .normal)
+       // aiyuButton(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.view.addSubview(aiyuButton)
+    }
+    func setPuddingButton() {
+        puddingButton = UIButton(frame: CGRect(x: ChoicesViewController.width * 9 / 12, y: ChoicesViewController.height / 7.0, width: ChoicesViewController.width / 5.0, height: ChoicesViewController.height / 7.0))
+        puddingButton.backgroundColor = UIColor.blue
+        puddingButton.layer.cornerRadius = 3.0
+        puddingButton.setTitle("Pudding", for: .normal)
+        // puddingButton(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.view.addSubview(puddingButton)
+    }
+    func setJellyButton() {
+        jellyButton = UIButton(frame: CGRect(x: ChoicesViewController.width / 12, y: ChoicesViewController.height * 4 / 7, width: ChoicesViewController.width / 5.0, height: ChoicesViewController.height / 7.0))
+        jellyButton.backgroundColor = UIColor.blue
+        jellyButton.layer.cornerRadius = 3.0
+        jellyButton.setTitle("Jelly", for: .normal)
+        // pearlButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.view.addSubview(jellyButton)
+    }
+    
+    
     
     @objc private func addEntryAction() {
         let entryViewController = MilkViewController()
