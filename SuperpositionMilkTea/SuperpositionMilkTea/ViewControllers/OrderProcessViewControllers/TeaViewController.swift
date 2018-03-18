ac9ce1928b7dea77769c2c611ef2b44a508333fa
Arleen Liu <arleenliu@gmail.com>
2018-03-18 06:56:05 -0700

    lkjl

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/OrderProcessViewControllers/TeaViewController.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/OrderProcessViewControllers/TeaViewController.swift
@@ -11,26 +11,50 @@ import UIKit
 
 class TeaViewController : ChoicesViewController /*UITableViewDelegate, UITableViewDataSource*/ {
     
+    var cup1 = CupView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Tea"
+        setupSlider()
+        self.view.backgroundColor = UIColor.white
         
-        super.setNextViewController(nextVC: TopViewController())
+        let addEntryButton = UIBarButtonItem(title: "Next", style: UIBarButtonItemStyle .plain, target: self, action: #selector(addEntryAction))
         
+        // UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addEntryAction))
+        self.navigationItem.rightBarButtonItem = addEntryButton
         
         // Add the view to the view hierarchy so that it shows up on screen
-        self.view.addSubview(ChoicesViewController.getCup())
+        self.view.addSubview(cup1.getCup())
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.view.addSubview(ChoicesViewController.getCup()) // maybe?
+        self.view.addSubview(cup1.getCup()) // maybe?
     }
     
-    override func sliderValueChanged(_ sender: UISlider!) {
-        super.sliderValueChanged(sender)
+    @objc private func addEntryAction() {
+        let entryViewController = TopViewController()
+        entryViewController.setCup(cv: cup1)
+        navigationController?.pushViewController(entryViewController, animated: true)
+    }
+    
+    func setupSlider() {
+        let slider = UISlider(frame: CGRect(x: 50, y: 550, width: 280, height: 20))
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.isContinuous = true
+        slider.value = 0
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+        self.view.addSubview(slider)
+    }
+    
+    @objc func sliderValueChanged(_ sender: UISlider!) {
+        cup1.setValues(percent: Double(sender.value), vc: self)
         
-        ChoicesViewController.setValues(percent: Double(sender.value), vc: self)
-        
+    }
+    
+    func setCup(cv: CupView) {
+        cup1 = cv
     }
 }
