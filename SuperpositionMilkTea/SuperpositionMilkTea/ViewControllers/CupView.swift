2e049ded40fc0bb2cbf3113a1e8f30a58a2f8279
Jannie Zhong <janniezhong@Jannies-MacBook-Pro.local>
2018-03-18 01:53:39 -0700

    yayyy

--- SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/CupView.swift
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/CupView.swift
@@ -22,6 +22,14 @@ class CupView : UIView /*UITableViewDelegate, UITableViewDataSource*/ {
     lazy var currX = frameX
     lazy var currY = frameY + frameHeight
     lazy var currHeight = 0.0
+    var count = 0
+    var botPercent = 0.0;
+    var milkPercent = 0.0;
+    var teaPercent = 0.0;
+    var topPercent = 0.0;
+    var icePercent = 0.0;
+    
+    var fillColor = UIColor.white
     
     override init(frame: CGRect) {
         super.init(frame: CGRect(x: frameX, y: frameY, width: frameWidth, height: frameHeight))
@@ -32,10 +40,47 @@ class CupView : UIView /*UITableViewDelegate, UITableViewDataSource*/ {
         super.init(coder: aDecoder)
     }
     
-    func drawRect(percent: Double, color: UIColor) {
+    func drawRect(percent: Double, color: UIColor, vc: UIViewController) {
         
         currHeight = (percent / 100.0)*frameHeight
         currY = currY-currHeight
+        
+        switch vc {
+        case is IceViewController:
+            fillColor = UIColor.purple
+            fillColor.setFill()
+            icePercent = (percent / 100.0)
+            count = 5;
+        case is TopViewController:
+            fillColor = UIColor.blue
+            fillColor.setFill()
+            topPercent = (percent / 100.0)
+            count = 4;
+        case is TeaViewController:
+            fillColor = UIColor.green
+            fillColor.setFill()
+            teaPercent = (percent / 100.0)
+            count = 3;
+        case is MilkViewController:
+            fillColor = UIColor.orange
+            fillColor.setFill()
+            milkPercent = (percent / 100.0)
+            count = 2;
+        case is BotViewController:
+            fillColor = UIColor.red
+            fillColor.setFill()
+            botPercent = (percent / 100.0)
+            count = 1;
+        default:
+            fillColor.setFill()
+        }
+        
+        var prevX = 0;
+        var prevY = 0;
+        
+        for i in 0..<count {
+            
+        }
         
         let rect = CGRect(x: currX, y: currY, width: frameWidth, height: currHeight)
         let myView = UIView(frame: rect)
