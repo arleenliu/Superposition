84679f8b03e355324162175aa41f96d4f4f391db
Arleen Liu <arleenliu@gmail.com>
2018-03-18 02:41:09 -0700

    tryyyyy

---
+++ SuperpositionMilkTea/SuperpositionMilkTea/ViewControllers/SideMenuViewControllers/LogoutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogoutViewController.swift
+//  SuperpositionMilkTea
+//
+//  Created by Arleen Liu on 3/18/18.
+//  Copyright © 2018 Jannie Zhong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogoutViewController : UIViewController /*UITableViewDelegate, UITableViewDataSource*/ {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
